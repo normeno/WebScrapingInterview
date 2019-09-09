9bbb8eb2f0065e10f439e2251bb24bd27525ca0e
Nicolas Ormeno <ni.ormeno@gmail.com>
2019-09-09 11:56:18 -0300

    Several Improvements

--- WebScrapingInterview/Controllers/ValuesController.cs
+++ WebScrapingInterview/Controllers/ValuesController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
+using WebScrapingInterview.Models;
 
 namespace WebScrapingInterview.Controllers
 {
@@ -10,11 +12,72 @@ namespace WebScrapingInterview.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly HtmlWeb _web;
+        private string _scrapingUrl;
+        private HtmlNode _mainNode;
+
+        public ValuesController()
+        {
+            _web = new HtmlWeb();
+            SetScrapingUrl();
+        }
+
+        // Set the URL to generate scraping
+        private void SetScrapingUrl()
+        {
+            _scrapingUrl = @"https://www.vacasa.com/vacation-rentals/chile/Santiago/Santiago/";
+        }
+
+        private void SetMainNodeByClassName(string el, string className)
+        {
+            var HtmlDoc = _web.Load(_scrapingUrl);
+            //_mainNode = HtmlDoc.DocumentNode.SelectNodes("//"+el+"[contains(@class, '"+className+"')]");
+            _mainNode = HtmlDoc.DocumentNode.SelectSingleNode("//" + el + "[contains(@class, '" + className + "')]");
+        }
+
+        private void SetMainNodeBId(string el, string id)
+        {
+            var HtmlDoc = _web.Load(_scrapingUrl);
+            _mainNode = HtmlDoc.DocumentNode.SelectSingleNode("//" + el + "[contains(@id, '" + id + "')]");
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<List<VacasaRentals>> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<VacasaRentals> scrapingElements = new List<VacasaRentals>();
+            var HtmlDoc = _web.Load(_scrapingUrl);
+
+            var mainNode = HtmlDoc.DocumentNode.SelectNodes("//div[contains(@class, 'unit-listing')]");
+
+            foreach (var nNode in mainNode)
+            {
+                try
+                {
+                    string id = nNode.Attributes["data-mini"].Value;
+                    string title = nNode.Element("h2").InnerText;
+                    string price = nNode.SelectSingleNode("//a[contains(@class, 'type-body-small') and contains(@href, '/unit.php?UnitID="+id+"')]").InnerText;
+                    string image = nNode.SelectSingleNode("//a[contains(@class, 'type-heading-small') and contains(@href, '/unit.php?UnitID=" + id+"')]/img").Attributes["src"].Value;
+                    string[] splitedPrice = price.Split("-");
+
+                    scrapingElements.Add(new VacasaRentals()
+                    {
+                        ID = int.Parse(id),
+                        Title = title,
+                        MinPrice = splitedPrice[0].Trim().Remove(0, 1),
+                        MaxPrice = splitedPrice[1].Trim().Remove(0, 1),
+                        MoneySign = price.Trim().Substring(0, 1), 
+                        Image = image.Remove(0, 2)
+                    });
+                }
+                catch(Exception ex)
+                {
+                    // TODO
+                    string errMessage = ex.Message;
+                }
+            }
+
+            return scrapingElements;
         }
 
         // GET api/values/5
---
+++ WebScrapingInterview/Models/VacasaRentals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace WebScrapingInterview.Models
+{
+    public class VacasaRentals
+    {
+        public VacasaRentals()
+        {
+        }
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string MinPrice { get; set; }
+        public string MaxPrice { get; set; }
+        public string MoneySign { get; set; }
+        public string Image { get; set; }
+    }
+}
